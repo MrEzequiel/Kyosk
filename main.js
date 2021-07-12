d22064947c114afd6d1d946e6b263cd1efa48603
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2021-07-11 22:09:49 -0500

    :sparkles: smooth commit

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ function accordion() {
 
 // event
 btns.forEach(el => el.addEventListener('click', accordion))
+
+/* SCROLL REVEAL*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '40px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `header main,
+  #about-us .image-about, #about-us text-about,
+  #membership .image-member, #membership .text-member,
+  #faqs .image-faq, #faqs .faq,
+  #contact-us .copyright, #contact-us .terms
+  `,
+  { interval: 100 }
+)
